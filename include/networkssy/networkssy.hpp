f1293dfb6d798d9c7ebc9e7b6ac6b98d3ee8f6fe
Pablo <pablohuggem@gmail.com>
2023-05-29 18:47:05 -0300

    refactor(networkssy.hpp): change connection class methods to use trailing return type syntax refactor(networkssy.hpp): make socket_fd protected instead of private feat(networkssy.hpp): add tcp_socket class with connect, listen, and accept methods The connection class methods were changed to use the trailing return type syntax for consistency with the rest of the codebase. The socket_fd member variable was changed from private to protected to allow for inheritance. The tcp_socket class was added with the connect, listen, and accept methods to allow for TCP socket communication.

--- include/networkssy/networkssy.hpp
+++ include/networkssy/networkssy.hpp
@@ -5,50 +5,12 @@
 #include <vector>
 
 namespace networkssy {
-/**
- * @brief Interface for network connections
- *
- * @details This class is an interface for network connections. It provides
- * methods to connect, disconnect, send and receive data.
- *
- */
+
 class connection {
 public:
-  /**
-   * @brief Connect to a host
-   *
-   * @details This method connects to a host.
-   *
-   * @param host The host to connect to
-   * @param port The port to connect to
-   */
-  virtual void connect(const std::string& host, uint16_t port) = 0;
-
-  /**
-   * @brief Disconnect from the host
-   *
-   * @details This method disconnects from the host.
-   *
-   */
-  virtual void disconnect() = 0;
-
-  /**
-   * @brief Send data to the host
-   *
-   * @details This method sends data to the host.
-   *
-   * @param data The data to send
-   */
-  virtual void send(const std::vector<uint8_t>& data) = 0;
-
-  /**
-   * @brief Receive data from the host
-   *
-   * @details This method receives data from the host.
-   *
-   * @param size The size of the data to receive
-   * @return The received data
-   */
+  virtual auto connect(const std::string& host, uint16_t port) -> void = 0;
+  virtual auto disconnect() -> void = 0;
+  virtual auto send(const std::vector<uint8_t>& data) -> void = 0;
   virtual auto receive(size_t size) -> std::vector<uint8_t> = 0;
 };
 
@@ -60,7 +22,18 @@ public:
   auto bind(const std::string& host, uint16_t port) const -> void;
   auto close() const -> void;
 
-private:
-  int socket_fd;
+protected:
+  int socket_fd; // NOLINT
 };
+
+class tcp_socket : public socket {
+public:
+  tcp_socket();
+  ~tcp_socket();
+
+  auto connect(const std::string& host, uint16_t port) -> void;
+  auto listen(int backlog) -> void;
+  auto accept() -> int;
+};
+
 } // namespace networkssy
--- src/networkssy/networkssy.cpp
+++ src/networkssy/networkssy.cpp
@@ -1,13 +1,13 @@
 #include <networkssy/networkssy.hpp>
-#include <string>
 
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <stdexcept>
+#include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-#include <stdexcept>
 
 namespace networkssy {
 
@@ -34,6 +34,37 @@ auto socket::bind(const std::string& host, uint16_t port) const -> void {
 
 auto socket::close() const -> void {
   ::close(socket_fd);
+}
+
+tcp_socket::tcp_socket() : socket(AF_INET, SOCK_STREAM, 0) {}
+
+tcp_socket::~tcp_socket() {
+  close();
+}
+
+auto tcp_socket::connect(const std::string& host, uint16_t port) -> void {
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_port = htons(port);
+  inet_pton(AF_INET, host.c_str(), &address.sin_addr);
+
+  if (::connect(socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    throw std::runtime_error("Could not connect socket");
+  }
+}
+
+auto tcp_socket::listen(int backlog) -> void {
+  if (::listen(socket_fd, backlog) < 0) {
+    throw std::runtime_error("Could not listen on socket");
+  }
+}
+
+auto tcp_socket::accept() -> int {
+  int new_socket_fd;
+  if ((new_socket_fd = ::accept(socket_fd, nullptr, nullptr)) < 0) {
+    throw std::runtime_error("Could not accept connection");
+  }
+  return new_socket_fd;
 }
 
 auto fn() -> std::string {

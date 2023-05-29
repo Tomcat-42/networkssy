21afa119fffe1ef89db9cf4635c9620e06f5d145
Pablo <pablohuggem@gmail.com>
2023-05-29 17:56:50 -0300

    style(.clang-format): change ConstructorInitializerIndentWidth and ContinuationIndentWidth to 2 docs(networkssy.hpp): add documentation to connection class and its methods The ConstructorInitializerIndentWidth and ContinuationIndentWidth were changed to 2 to improve code readability. The connection class was documented to provide a clear understanding of its purpose and methods. The methods connect, disconnect, send, and receive were documented to provide a clear understanding of their functionality.

--- include/networkssy/networkssy.hpp
+++ include/networkssy/networkssy.hpp
@@ -1,12 +1,54 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
+#include <vector>
 
-namespace networkssy  {
+namespace networkssy {
 /**
-  * @brief  Brief explanation of the function
-  * @param  Parameter(s) description
-  * @return Return value description
-  */
-auto fn() -> std::string;
+ * @brief Interface for network connections
+ *
+ * @details This class is an interface for network connections. It provides
+ * methods to connect, disconnect, send and receive data.
+ *
+ */
+class connection {
+public:
+  /**
+   * @brief Connect to a host
+   *
+   * @details This method connects to a host.
+   *
+   * @param host The host to connect to
+   * @param port The port to connect to
+   */
+  virtual void connect(const std::string& host, uint16_t port) = 0;
+
+  /**
+   * @brief Disconnect from the host
+   *
+   * @details This method disconnects from the host.
+   *
+   */
+  virtual void disconnect() = 0;
+
+  /**
+   * @brief Send data to the host
+   *
+   * @details This method sends data to the host.
+   *
+   * @param data The data to send
+   */
+  virtual void send(const std::vector<uint8_t>& data) = 0;
+
+  /**
+   * @brief Receive data from the host
+   *
+   * @details This method receives data from the host.
+   *
+   * @param size The size of the data to receive
+   * @return The received data
+   */
+  virtual auto receive(size_t size) -> std::vector<uint8_t> = 0;
+};
 } // namespace networkssy

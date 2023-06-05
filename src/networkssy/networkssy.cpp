2f8686886c7e0fb26ebfc85ca8a1291e518a60ec
Pablo <pablo.hugen@bry.com.br>
2023-06-05 16:14:58 -0300

    refactor(networkssy): use std::unique_ptr to manage socket file descriptor The socket file descriptor is now managed by a std::unique_ptr, which ensures that the socket is closed when the object is destroyed. This improves the safety of the code and reduces the risk of resource leaks. The socket constructor and bind, close, and get_socketfd methods have been updated to use the new unique_ptr.

--- src/networkssy/networkssy.cpp
+++ src/networkssy/networkssy.cpp
@@ -13,34 +13,40 @@
 
 namespace networkssy {
 
-
-socket::socket(int domain, int type, int protocol) {
-  if ((socket_fd = ::socket(domain, type, protocol)) < 0) {
+socket::socket(const int domain, const int type, const int protocol) {
+  const int _socket_fd = ::socket(domain, type, protocol);
+  if (_socket_fd < 0) {
     throw std::runtime_error("Could not create socket");
   }
+
+  socket_fd = std::unique_ptr<const int, std::function<void(const int*)>>(
+    new int(_socket_fd), [](const int* socket_fd) {
+      ::close(*socket_fd);
+      delete socket_fd;
+    });
 }
 
 socket::~socket() {
   close();
 }
 
-auto socket::bind(const std::string& host, uint16_t port) const -> void {
+auto socket::bind(const std::string& host, const uint16_t port) const -> void {
   struct sockaddr_in address;
   address.sin_family = AF_INET;
   address.sin_port = htons(port);
   inet_pton(AF_INET, host.c_str(), &address.sin_addr);
 
-  if (::bind(socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+  if (::bind(*socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
     throw std::runtime_error("Could not bind socket");
   }
 }
 
 auto socket::close() const -> void {
-  ::close(socket_fd);
+  ::close(*socket_fd);
 }
 
 auto socket::get_socketfd() const -> int {
-  return socket_fd;
+  return *socket_fd;
 }
 
 tcp_socket::tcp_socket() : socket(AF_INET, SOCK_STREAM, 0) {}
@@ -55,20 +61,20 @@ auto tcp_socket::connect(const std::string& host, uint16_t port) -> void {
   address.sin_port = htons(port);
   inet_pton(AF_INET, host.c_str(), &address.sin_addr);
 
-  if (::connect(socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+  if (::connect(*socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
     throw std::runtime_error("Could not connect socket");
   }
 }
 
 auto tcp_socket::listen(int backlog) -> void {
-  if (::listen(socket_fd, backlog) < 0) {
+  if (::listen(*socket_fd, backlog) < 0) {
     throw std::runtime_error("Could not listen on socket");
   }
 }
 
 auto tcp_socket::accept() -> int {
   int new_socket_fd;
-  if ((new_socket_fd = ::accept(socket_fd, nullptr, nullptr)) < 0) {
+  if ((new_socket_fd = ::accept(*socket_fd, nullptr, nullptr)) < 0) {
     throw std::runtime_error("Could not accept connection");
   }
   return new_socket_fd;
@@ -83,7 +89,7 @@ auto udp_socket::set_destination(const std::string& host, uint16_t port)
   address.sin_port = htons(port);
   inet_pton(AF_INET, host.c_str(), &address.sin_addr);
 
-  if (::connect(socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+  if (::connect(*socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
     throw std::runtime_error("Could not connect socket");
   }
 }

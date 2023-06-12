0489dcadaa3b6ed63a2c8531437598a6e629d7f9
Pablo <pablo.hugen@bry.com.br>
2023-06-12 15:15:49 -0300

    refactor(networkssy.hpp): rename connect method to connect_to_server and add start_server and accept_client methods to tcp_connection and udp_connection classes feat(networkssy.hpp): add send_to and receive_from methods to udp_connection class feat(networkssy.hpp): add send_with_ack and receive_with_ack methods to udp_connection class refactor(networkssy.hpp): add virtual destructor to connection class refactor(networkssy.hpp): add set_socketfd method to socket class refactor(main.cpp): add command line argument parsing using cxxopts library feat(main.cpp): add support for UDP protocol and guarantee delivery option The changes in networkssy.hpp add more functionality to the connection classes, allowing for more flexibility in how connections are established and data is sent and received. The addition of the virtual destructor to the connection class ensures that derived classes are properly destroyed when deleted through a pointer to the base class. The addition of the set_socketfd method to the
    feat(networkssy): add support for UDP protocol and guarantee delivery option
    feat(serverssy): add command line options for protocol, guarantee delivery, block size, and port
    This commit adds support for the UDP protocol in the networkssy library, including the ability to set the destination for a UDP socket and receive data with or without an ACK. In the serverssy application, command line options have been added to specify the protocol to use (TCP or UDP), whether to guarantee delivery if UDP is used, the block size for receiving data, and the port to listen on. The application now starts a server and accepts a client connection, then receives data from the client and writes it to stdout.

--- include/networkssy/networkssy.hpp
+++ include/networkssy/networkssy.hpp
@@ -10,10 +10,14 @@ namespace networkssy {
 
 class connection {
 public:
-  virtual auto connect(const std::string& host, uint16_t port) -> void = 0;
+  virtual auto connect_to_server(const std::string& host, uint16_t port)
+    -> void = 0;
+  virtual auto start_server(uint16_t port) -> void = 0;
+  virtual auto accept_client() -> void = 0;
   virtual auto disconnect() -> void = 0;
   virtual auto send(const std::vector<uint8_t>& data) -> void = 0;
   virtual auto receive(size_t size) -> std::vector<uint8_t> = 0;
+  virtual ~connection() = default;
 };
 
 class socket {
@@ -24,6 +28,7 @@ public:
   auto bind(const std::string& host, uint16_t port) const -> void;
   auto close() const -> void;
   [[nodiscard]] auto get_socketfd() const -> int;
+  auto set_socketfd(int new_socketfd) -> void;
 
 protected:
   std::unique_ptr<const int, std::function<void(const int*)>>
@@ -46,16 +51,24 @@ public:
   ~udp_socket();
 
   auto set_destination(const std::string& host, uint16_t port) -> void;
+  auto receive_from(size_t size)
+    -> std::pair<std::vector<uint8_t>, std::pair<std::string, uint16_t>>;
+  auto send_to(const std::vector<uint8_t>& data, const std::string& host,
+               uint16_t port) -> void;
 };
 
 class tcp_connection : public connection {
 public:
   tcp_connection();
 
-  auto connect(const std::string& host, uint16_t port) -> void override;
+  auto connect_to_server(const std::string& host, uint16_t port)
+    -> void override;
+  auto start_server(uint16_t port) -> void override;
+  auto accept_client() -> void override;
   auto disconnect() -> void override;
   auto send(const std::vector<uint8_t>& data) -> void override;
   auto receive(size_t size) -> std::vector<uint8_t> override;
+  ~tcp_connection() override = default;
 
 private:
   tcp_socket socket;
@@ -65,17 +78,28 @@ class udp_connection : public connection {
 public:
   udp_connection();
 
-  auto connect(const std::string& host, uint16_t port) -> void override;
+  auto connect_to_server(const std::string& host, uint16_t port)
+    -> void override;
+  auto start_server(uint16_t port) -> void override;
+  auto accept_client() -> void override;
   auto disconnect() -> void override;
   auto send(const std::vector<uint8_t>& data) -> void override;
   auto receive(size_t size) -> std::vector<uint8_t> override;
-  auto send_with_ack(const std::vector<uint8_t>& data) -> void;
-  auto receive_with_ack(size_t size) -> std::vector<uint8_t>;
+  auto send_to(const std::vector<uint8_t>& data, const std::string& host,
+               uint16_t port) -> void;
+  auto receive_from(size_t size)
+    -> std::pair<std::vector<uint8_t>, std::pair<std::string, uint16_t>>;
+  auto send_with_ack(const std::vector<uint8_t>& data, const std::string& host,
+                     uint16_t port) -> void;
+  auto receive_with_ack(size_t size)
+    -> std::pair<std::vector<uint8_t>, std::pair<std::string, uint16_t>>;
+  ~udp_connection() override = default;
 
 private:
   udp_socket socket;
   static constexpr uint8_t ACK = 0x01;
   static constexpr size_t ACK_SIZE = 1;
   static constexpr uint8_t ACK_TIMEOUT = 3;
+  static constexpr uint8_t RETRY_DELAY_MS = 100;
 };
 } // namespace networkssy
--- src/serverssy/main.cpp
+++ src/serverssy/main.cpp
@@ -1,6 +1,81 @@
+#include <cxxopts.hpp>
 #include <iostream>
+#include <networkssy/networkssy.hpp>
 
-auto main() -> int{
-  std::cout << "Hello, World!\n";
+auto main(int argc, char* argv[]) -> int {
+  cxxopts::Options options("serverssy",
+                           "Networkssy server for receiving files.");
+
+  options.add_options()("p,protocol",
+                        "Protocol to use (tcp or udp), default is tcp",
+                        cxxopts::value<std::string>()->default_value("tcp"))(
+    "g,guarantee", "Guarantee delivery if UDP is used (default is no)",
+    cxxopts::value<bool>()->default_value("false"))(
+    "b,block", "Block size, default is 1000 bytes",
+    cxxopts::value<int>()->default_value("1000"))(
+    "port", "Server port", cxxopts::value<uint16_t>())("h,help", "Print help");
+
+  options.parse_positional({"port"});
+
+  try {
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help") != 0U) {
+      std::cout << options.help() << std::endl;
+      return 0;
+    }
+
+    std::string protocol = result["protocol"].as<std::string>();
+    bool guarantee = result["guarantee"].as<bool>();
+    int block_size = result["block"].as<int>();
+    uint16_t port = result["port"].as<uint16_t>();
+
+    std::cout << protocol << std::endl;
+
+    std::unique_ptr<networkssy::connection> conn;
+    if (protocol == "tcp") {
+      conn = std::make_unique<networkssy::tcp_connection>();
+    } else if (protocol == "udp") {
+      conn = std::make_unique<networkssy::udp_connection>();
+    } else {
+      std::cerr << "Invalid protocol. Use tcp or udp." << std::endl;
+      return 1;
+    }
+
+    std::cout << "Server is starting on port " << port << std::endl;
+    conn->start_server(port);
+    std::cout << "Server started" << std::endl;
+
+    conn->accept_client();
+    std::cout << "Client accepted" << std::endl;
+
+    while (true) {
+      std::vector<uint8_t> buffer;
+      if (guarantee && protocol == "udp") {
+        auto received = dynamic_cast<networkssy::udp_connection*>(conn.get())
+                          ->receive_with_ack(block_size);
+        buffer = std::move(received.first);
+      } else {
+        std::cout << "Receiving..." << std::endl;
+        buffer = conn->receive(block_size);
+      }
+
+      if (buffer.empty()) {
+        break;
+      }
+
+      std::cout.write(reinterpret_cast<char*>(buffer.data()), buffer.size());
+    }
+
+    conn->disconnect();
+  } catch (const cxxopts::exceptions::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    std::cerr << options.help() << std::endl;
+    return 1;
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
+
   return 0;
 }
